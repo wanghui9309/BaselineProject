4b9a752bb64fb4b19ca341291b5987cb44bb3691
WangHui <wanghui9309@gmail.com>
2018-12-27 15:47:51 +0800

    feat: APNS

--- BaselineProject/Supporting Files/AppDelegate.m
+++ BaselineProject/Supporting Files/AppDelegate.m
@@ -10,12 +10,13 @@
 
 #import "UIWindow+Extension.h"
 
-@interface AppDelegate ()
+#import <UserNotifications/UserNotifications.h>
+
+@interface AppDelegate ()<UNUserNotificationCenterDelegate>
 
 @end
 
 @implementation AppDelegate
-
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -45,13 +46,13 @@
     return YES;
 }
 
-//app 将要失去焦点
+// app 将要失去焦点
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     
 }
 
-//app 已经进入后台
+// app 已经进入后台
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
     
@@ -63,16 +64,50 @@
     
 }
 
-//app 已经获得焦点
+// app 已经获得焦点
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     
 }
 
-//app 将要终止
+// app 将要终止
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     
 }
+
+#pragma mark - APNS
+// 注册成功回调方法，其中deviceToken即为APNs返回的token
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    // 发送deviceToken到你的服务器端
+    NSLog(@"deviceToken:%@", deviceToken);
+}
+
+// 应用处于前台状态触发, 获取远程推送 兼容 iOS 7 以下推送环境
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    
+}
+
+/**
+ iOS 7 以后远程推送触发，不管在前台还是后台
+ 此方法需要配置后台模式并且在推送负载中必须有 "content-available" : 1
+*/
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))completionHandler
+{
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler API_AVAILABLE(ios(10.0))
+{
+    completionHandler(UNNotificationPresentationOptionBadge | UNNotificationPresentationOptionSound | UNNotificationPresentationOptionAlert);
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler API_AVAILABLE(ios(10.0))
+{
+    completionHandler();
+}
+
 
 @end
